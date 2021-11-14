82417e1b9dba1e392079cd4e39251526efe8095d
Pingutino <nl^fd97F4kKK>
2021-11-14 18:29:43 +0100

    Completed template

---
+++ src/db/db.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+const DB_NAME = "my-database";
+
+export const db = {
+    _dbCLient: null,
+    connect: async function(url) {
+        const client = await MongoClient.connect(url, {
+            maxPoolSize: 10,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        this._dbCLient = client;
+    },
+    getConnection: function() {
+        if (!this._dbCLient){
+            console.log("You need to call the connect function first");
+            process.exit(1);
+        }
+        
+        return this._dbCLient.db(DB_NAME);
+    }
+}
